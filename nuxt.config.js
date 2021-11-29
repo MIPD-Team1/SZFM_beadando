6cc097444154a76d7a7376cb18424222096a150e
NincsJobbOtletem <patrikvok123312@gmail.com>
2021-11-29 16:41:58 +0100

    UI Update animation

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'SZFM_beadando',
+    title: 'SZFM_Bolt',
     htmlAttrs: {
       lang: 'en'
     },
@@ -9,19 +9,34 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { name: 'format-detection', content: 'SZFM beadando project!' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/297.jpg' }
     ]
   },
+  loading: { // nem müködik :D
+    color: 'yellow',
+    height: '5px',
+    duration: 50},
+
+  loadingIndicator: {// nem müködik :D
+    name: 'circle',
+    color: '#3B8070',
+    background: 'yellow'
+  },
+  
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: ["~assets/styles/main.css"],
+  transition: {
+    name: "fade",
+    mode: "out-in"
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~plugins/date-filter.js',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -33,14 +48,13 @@ export default {
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
-    // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
   ],
-
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
-  axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+  router:{
+    middleware: 'log'
   }
+  
 }
